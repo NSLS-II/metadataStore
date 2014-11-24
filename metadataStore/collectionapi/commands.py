72e3dd61eed7c657869abf57db876c04b33c2c87
Thomas A Caswell <tcaswell@bnl.gov>
2014-11-24 14:04:41 -0500

    MNT/API : moved validate_dict_keys
    moved from collectionapi/commands.py to utilities/__init__.py.
    
    Moved the init level in anticipation of flattening this namespace.

--- metadataStore/collectionapi/commands.py
+++ metadataStore/collectionapi/commands.py
@@ -2,35 +2,7 @@ __author__ = 'arkilic'
 
 import getpass
 from metadataStore.dataapi.commands import insert_event
-
-
-# this needs to be moved up a level or three
-def validate_dict_keys(input_dict, req_set):
-    """
-    Validate that the required keys are in the input dictionary.
-
-    This function returns None if the dict is valid, and raises
-    `ValueError` if it is not.
-
-    Parameters
-    ----------
-    input_dict : dict
-        The dictionary to have it's keys validate
-
-    req_set : iterable
-        The keys that must be in the dictionary
-
-    Raises
-    ------
-    `ValueError` if any of the required keys are missing
-    """
-    missing = []
-    for k in req_set:
-        if k not in input_dict:
-            missing.append(k)
-    if len(missing):
-        missing = ', '.join('{}'.format(k) for k in missing)
-        raise ValueError("The required key(s) {} are missing".format(missing))
+from metadataStore.utilities import validate_dict_keys
 
 
 def create_event(event):
